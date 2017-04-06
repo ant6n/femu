bd038191f9b74b49207694b8337024e9d9e68dce
Anton Dubrau <ant6n@thetransitapp.com>
2017-04-06 17:33:58 -0400

    change main.cpp: target gets injected in emu now

--- source/main.cpp
+++ source/main.cpp
@@ -21,8 +21,8 @@
 const std::string emulatorElfPath = "femu-inject";
 const std::string combinedElfPath = "modified-elf";
 
-elf::ElfFile injectElf(const elf::ElfFile& inputElf, const elf::ElfFile& injectElf);
-bool injectEmuData(const elf::ElfFile& inputElf, elf::ElfFile& injectElf);
+elf::ElfFile injectElf(const elf::ElfFile& targetElf, const elf::ElfFile& injectElf);
+bool injectEmuData(elf::ElfFile& emuElf, const elf::ElfFile& targetElf);
 
 
 /** returns the smallest number n >= offset s.t. n % alignment == 0 */
@@ -33,25 +33,25 @@ static size_t align(int offset, int alignment) {
     return result;
 }
 
-static bool injectEmulator(const std::string& inputPath, const std::string& outputPath) {
-    elf::ElfFile inputElf(inputPath);
+/** create emulator for the spcified elf defined by the target path, by injecting it into the emu */
+static bool createEmulator(const std::string& targetPath, const std::string& outputPath) {
+    elf::ElfFile targetElf(targetPath);
     elf::ElfFile emuElf(emulatorElfPath);
     
-    printf("elf:\n");
-    //inputElf.printHeader();
-    inputElf.printProgramHeaders();
-    printf("inject:\n");
-    //injectElf.printHeader();
+    printf("target elf:\n");
+    targetElf.printProgramHeaders();
+    printf("emu elf:\n");
     emuElf.printProgramHeaders();
 
-    if (not injectEmuData(inputElf, emuElf)) return false;
+    if (not injectEmuData(emuElf, targetElf)) return false;
     
-    elf::ElfFile outputElf = injectElf(inputElf, emuElf);
+    elf::ElfFile outputElf = injectElf(emuElf, targetElf);
     std::cout << "write file: " << outputPath << std::endl;
     return outputElf.writeToFile(outputPath);
 }
 
-elf::ElfFile injectElf(const elf::ElfFile& inputElf, const elf::ElfFile& injectElf) {
+/** append the given injectElf into the targetElf and return new elf */
+elf::ElfFile injectElf(const elf::ElfFile& targetElf, const elf::ElfFile& injectElf) {
     
     // get alignment of inect elf
     int injectElfAlignment = 1;
@@ -67,27 +67,27 @@ elf::ElfFile injectElf(const elf::ElfFile& inputElf, const elf::ElfFile& injectE
     }
 
     // collect offsets and sizes
-    size_t injectElfOffset = align(inputElf.size(), injectElfAlignment);
-    size_t programHeaderOffset = align(injectElfOffset + injectElf.size(), alignof(Elf32_Phdr));
-    size_t newProgramHeaderSize = inputElf.header().e_phentsize*(inputElf.numSegments() +
+    size_t injectElfOffset = align(targetElf.size(), injectElfAlignment);
+    size_t newProgramHeaderOffset = align(injectElfOffset + injectElf.size(), alignof(Elf32_Phdr));
+    size_t newProgramHeaderSize = targetElf.header().e_phentsize*(targetElf.numSegments() +
 																 injectElf.numSegments());
-    size_t newSize = programHeaderOffset + newProgramHeaderSize;
+    size_t newSize = newProgramHeaderOffset + newProgramHeaderSize;
     
     // create new data
     printf("new alloc: %d\n", newSize);
     char* newData = new char [newSize];
 
     // copy elfs
-    memcpy(newData, inputElf.data(), inputElf.size());
+    memcpy(newData, targetElf.data(), targetElf.size());
     memcpy(newData + injectElfOffset, injectElf.data(), injectElf.size());
     
-    // copy segment headers from output elf
-    size_t inputPHSize = inputElf.header().e_phentsize * inputElf.numSegments();
-    memcpy(newData + programHeaderOffset, inputElf.data() + inputElf.header().e_phoff, inputPHSize);
+    // copy program headers from output elf
+    size_t inputPHSize = targetElf.header().e_phentsize * targetElf.numSegments();
+    memcpy(newData + newProgramHeaderOffset, targetElf.data() + targetElf.header().e_phoff, inputPHSize);
     
     // copy program headers from inject elf
-    size_t copyPHEntrySize = std::min(inputElf.header().e_phentsize, injectElf.header().e_phentsize);
-    size_t headerOffset = programHeaderOffset + inputPHSize;
+    size_t copyPHEntrySize = std::min(targetElf.header().e_phentsize, injectElf.header().e_phentsize);
+    size_t headerOffset = newProgramHeaderOffset + inputPHSize;
     for (int i = 0; i < injectElf.numSegments(); i++) {
         printf("add segment %d, offset: %d, \n", i, headerOffset);
         // copy program header entry
@@ -101,20 +101,16 @@ elf::ElfFile injectElf(const elf::ElfFile& inputElf, const elf::ElfFile& injectE
 			headerEntry->p_type = PT_NULL;
 		}
 	
-		headerOffset += inputElf.header().e_phentsize;
+		headerOffset += targetElf.header().e_phentsize;
     }
         
     // create new elf and update/fix header
     std::shared_ptr<char> shared(newData);
     elf::ElfFile outElf(shared, newSize);
-    outElf.header().e_machine = EM_ARM;
-    outElf.header().e_phoff = programHeaderOffset;
-    outElf.header().e_phnum = inputElf.numSegments() + injectElf.numSegments();
-    outElf.header().e_entry = injectElf.header().e_entry;
-    outElf.header().e_shoff = 0;
-    outElf.header().e_shnum = 0;
-    outElf.header().e_shstrndx = 0;
-    
+	
+    outElf.header().e_phoff = newProgramHeaderOffset;
+    outElf.header().e_phnum = targetElf.numSegments() + injectElf.numSegments();
+	
     printf("output:\n");
     outElf.printHeader();
     outElf.printProgramHeaders();
@@ -122,16 +118,16 @@ elf::ElfFile injectElf(const elf::ElfFile& inputElf, const elf::ElfFile& injectE
 }
 
 
-/** inject emulator data into the inject elf */
-bool injectEmuData(const elf::ElfFile& inputElf, elf::ElfFile& injectElf) {
+/** inject emulator data from target into the emulator elf */
+bool injectEmuData(elf::ElfFile& emuElf, const elf::ElfFile& targetElf) {
     // find R/W load segment with the magic number
     uintptr_t MAGIC = 0xbe61be61;
-    for (int i = 0; i < injectElf.numSegments(); i++) {
-        auto segment = injectElf.getSegment(i);
+    for (int i = 0; i < emuElf.numSegments(); i++) {
+        auto segment = emuElf.getSegment(i);
 		if (segment.header().p_type == PT_LOAD and
 			segment.header().p_flags == (PF_R | PF_W)) {
 			printf("searching for entry point in segment %d\n", i);
-			uintptr_t* pointer = (uintptr_t*)(injectElf.data() + segment.header().p_offset);
+			uintptr_t* pointer = (uintptr_t*)(emuElf.data() + segment.header().p_offset);
 			i = 0;
 			while (*pointer != MAGIC) {
 				if (i > segment.header().p_filesz) {
@@ -142,7 +138,7 @@ bool injectEmuData(const elf::ElfFile& inputElf, elf::ElfFile& injectElf) {
 				i += sizeof(uintptr_t);
 			}
 			printf("magic entrypoint found at offset: %d\n", i * sizeof(uintptr_t));
-			*pointer = inputElf.header().e_entry;
+			*pointer = targetElf.header().e_entry;
 			return true;
 		}
     }
@@ -152,7 +148,7 @@ bool injectEmuData(const elf::ElfFile& inputElf, elf::ElfFile& injectElf) {
 
 int main(int argc, char *const argv[], char *const envp[]) {
     std::string path = argv[1];
-    if (not injectEmulator(path, combinedElfPath)) {
+    if (not createEmulator(path, combinedElfPath)) {
 		std::cerr << "failed to inject emulator into program" << std::endl;
 		exit(EXIT_FAILURE);
     }
